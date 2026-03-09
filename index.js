a69a233551b84283ef999c72383245e2fd15a2bb
Daniella <daniella.abi.nader@gmail.com>
2026-03-09 20:13:48 +0200

    Compressed Audio

--- index.js
+++ index.js
@@ -15,131 +15,17 @@ lib.ssMetadata = [
 (lib.AnMovieClip = function(){
 	this.actionFrames = [];
 	this.ignorePause = false;
-	this.currentSoundStreamInMovieclip;
-	this.soundStreamDuration = new Map();
-	this.streamSoundSymbolsList = [];
-
-	this.gotoAndPlayForStreamSoundSync = function(positionOrLabel){
-		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
-	}
 	this.gotoAndPlay = function(positionOrLabel){
-		this.clearAllSoundStreams();
-		var pos = this.timeline.resolve(positionOrLabel);
-		if (pos != null) { this.startStreamSoundsForTargetedFrame(pos); }
 		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
 	}
 	this.play = function(){
-		this.clearAllSoundStreams();
-		this.startStreamSoundsForTargetedFrame(this.currentFrame);
 		cjs.MovieClip.prototype.play.call(this);
 	}
 	this.gotoAndStop = function(positionOrLabel){
 		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
-		this.clearAllSoundStreams();
 	}
 	this.stop = function(){
 		cjs.MovieClip.prototype.stop.call(this);
-		this.clearAllSoundStreams();
-	}
-	this.startStreamSoundsForTargetedFrame = function(targetFrame){
-		for(var index=0; index<this.streamSoundSymbolsList.length; index++){
-			if(index <= targetFrame && this.streamSoundSymbolsList[index] != undefined){
-				for(var i=0; i<this.streamSoundSymbolsList[index].length; i++){
-					var sound = this.streamSoundSymbolsList[index][i];
-					if(sound.endFrame > targetFrame){
-						var targetPosition = Math.abs((((targetFrame - sound.startFrame)/lib.properties.fps) * 1000));
-						var instance = playSound(sound.id);
-						var remainingLoop = 0;
-						if(sound.offset){
-							targetPosition = targetPosition + sound.offset;
-						}
-						else if(sound.loop > 1){
-							var loop = targetPosition /instance.duration;
-							remainingLoop = Math.floor(sound.loop - loop);
-							if(targetPosition == 0){ remainingLoop -= 1; }
-							targetPosition = targetPosition % instance.duration;
-						}
-						instance.loop = remainingLoop;
-						instance.position = Math.round(targetPosition);
-						this.InsertIntoSoundStreamData(instance, sound.startFrame, sound.endFrame, sound.loop , sound.offset);
-					}
-				}
-			}
-		}
-	}
-	this.InsertIntoSoundStreamData = function(soundInstance, startIndex, endIndex, loopValue, offsetValue){ 
- 		this.soundStreamDuration.set({instance:soundInstance}, {start: startIndex, end:endIndex, loop:loopValue, offset:offsetValue});
-	}
-	this.clearAllSoundStreams = function(){
-		this.soundStreamDuration.forEach(function(value,key){
-			key.instance.stop();
-		});
- 		this.soundStreamDuration.clear();
-		this.currentSoundStreamInMovieclip = undefined;
-	}
-	this.stopSoundStreams = function(currentFrame){
-		if(this.soundStreamDuration.size > 0){
-			var _this = this;
-			this.soundStreamDuration.forEach(function(value,key,arr){
-				if((value.end) == currentFrame){
-					key.instance.stop();
-					if(_this.currentSoundStreamInMovieclip == key) { _this.currentSoundStreamInMovieclip = undefined; }
-					arr.delete(key);
-				}
-			});
-		}
-	}
-
-	this.computeCurrentSoundStreamInstance = function(currentFrame){
-		if(this.currentSoundStreamInMovieclip == undefined){
-			var _this = this;
-			if(this.soundStreamDuration.size > 0){
-				var maxDuration = 0;
-				this.soundStreamDuration.forEach(function(value,key){
-					if(value.end > maxDuration){
-						maxDuration = value.end;
-						_this.currentSoundStreamInMovieclip = key;
-					}
-				});
-			}
-		}
-	}
-	this.getDesiredFrame = function(currentFrame, calculatedDesiredFrame){
-		for(var frameIndex in this.actionFrames){
-			if((frameIndex > currentFrame) && (frameIndex < calculatedDesiredFrame)){
-				return frameIndex;
-			}
-		}
-		return calculatedDesiredFrame;
-	}
-
-	this.syncStreamSounds = function(){
-		this.stopSoundStreams(this.currentFrame);
-		this.computeCurrentSoundStreamInstance(this.currentFrame);
-		if(this.currentSoundStreamInMovieclip != undefined){
-			var soundInstance = this.currentSoundStreamInMovieclip.instance;
-			if(soundInstance.position != 0){
-				var soundValue = this.soundStreamDuration.get(this.currentSoundStreamInMovieclip);
-				var soundPosition = (soundValue.offset?(soundInstance.position - soundValue.offset): soundInstance.position);
-				var calculatedDesiredFrame = (soundValue.start)+((soundPosition/1000) * lib.properties.fps);
-				if(soundValue.loop > 1){
-					calculatedDesiredFrame +=(((((soundValue.loop - soundInstance.loop -1)*soundInstance.duration)) / 1000) * lib.properties.fps);
-				}
-				calculatedDesiredFrame = Math.floor(calculatedDesiredFrame);
-				var deltaFrame = calculatedDesiredFrame - this.currentFrame;
-				if((deltaFrame >= 0) && this.ignorePause){
-					cjs.MovieClip.prototype.play.call(this);
-					this.ignorePause = false;
-				}
-				else if(deltaFrame >= 2){
-					this.gotoAndPlayForStreamSoundSync(this.getDesiredFrame(this.currentFrame,calculatedDesiredFrame));
-				}
-				else if(deltaFrame <= -2){
-					cjs.MovieClip.prototype.stop.call(this);
-					this.ignorePause = true;
-				}
-			}
-		}
 	}
 }).prototype = p = new cjs.MovieClip();
 // symbols:
@@ -1004,13 +890,8 @@ if (reversed == null) { reversed = false; }
 	cjs.MovieClip.apply(this,[props]);
 
 	this.actionFrames = [0,139,384,525,1011,1169];
-	this.streamSoundSymbolsList[0] = [{id:"weddingmusicwav",startFrame:0,endFrame:1170,loop:1,offset:0}];
 	// timeline functions:
 	this.frame_0 = function() {
-		this.clearAllSoundStreams();
-		 
-		var soundInstance = playSound("weddingmusicwav",0);
-		this.InsertIntoSoundStreamData(soundInstance,0,1170,1);
 		var _this = this;
 		/*
 		Stop a Movie Clip/Video
@@ -1030,6 +911,7 @@ if (reversed == null) { reversed = false; }
 		*/
 		_this.play();
 		});
+		playSound("weddingmusicwav");
 	}
 	this.frame_139 = function() {
 		var _this = this;
@@ -1330,13 +1212,13 @@ lib.properties = {
 	color: "#F3F3F3",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/index_atlas_P_1.png?1773077580737", id:"index_atlas_P_1"},
-		{src:"images/index_atlas_P_2.png?1773077580737", id:"index_atlas_P_2"},
-		{src:"images/index_atlas_NP_1.jpg?1773077580737", id:"index_atlas_NP_1"},
-		{src:"images/index_atlas_NP_2.jpg?1773077580737", id:"index_atlas_NP_2"},
-		{src:"images/index_atlas_NP_3.jpg?1773077580737", id:"index_atlas_NP_3"},
-		{src:"images/index_atlas_NP_4.jpg?1773077580737", id:"index_atlas_NP_4"},
-		{src:"sounds/weddingmusicwav.mp3?1773077580821", id:"weddingmusicwav"}
+		{src:"images/index_atlas_P_1.png", id:"index_atlas_P_1"},
+		{src:"images/index_atlas_P_2.png", id:"index_atlas_P_2"},
+		{src:"images/index_atlas_NP_1.jpg", id:"index_atlas_NP_1"},
+		{src:"images/index_atlas_NP_2.jpg", id:"index_atlas_NP_2"},
+		{src:"images/index_atlas_NP_3.jpg", id:"index_atlas_NP_3"},
+		{src:"images/index_atlas_NP_4.jpg", id:"index_atlas_NP_4"},
+		{src:"sounds/weddingmusicwav.mp3", id:"weddingmusicwav"}
 	],
 	preloads: []
 };
